65e3df35971b12ade98350c08426a2802f164a4f
Sachin <homesachin@M3-Max.local>
2024-01-22 22:03:57 +0530

    typing commit message is most boring part

--- form1/models.py
+++ form1/models.py
@@ -1,3 +1,4 @@
+
 from django.contrib.auth.hashers import make_password
 from django.db import models
 
--- form1/urls.py
+++ form1/urls.py
@@ -1,6 +1,5 @@
 
 from django.urls import path
-from form1.views import register
 
 from . import views
 
