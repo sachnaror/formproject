9c3ac54dcd5b0833d12e41e3bb89a7e758da9c2b
Sachin <homesachin@M3-Max.local>
2024-01-22 20:25:32 +0530

    typing commit message is most boring part

--- form1/models.py
+++ form1/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.hashers import make_password
 from django.db import models
 
-# Create your models here.
+
+class UserRegistration(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(UserRegistration, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
