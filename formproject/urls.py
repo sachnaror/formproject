1ca94cdd50a47db4ded92a61abfdb1c3f3e44ff7
Sachin <homesachin@M3-Max.local>
2024-01-23 12:56:06 +0530

    typing commit message is most boring part

--- formproject/urls.py
+++ formproject/urls.py
@@ -5,5 +5,7 @@ from form1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('tab1/', views.tab1, name='tab1'),
 ]
