3a65fe2793dc42ded22eee85f8e9e1d1400937bc
Sachin <homesachin@M3-Max.local>
2024-01-23 11:36:25 +0530

    typing commit message is most boring part

--- form1/models.py
+++ form1/models.py
@@ -1,15 +1,36 @@
-
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
-class UserRegistration(models.Model):
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-    def save(self, *args, **kwargs):
-        self.password = make_password(self.password)
-        super(UserRegistration, self).save(*args, **kwargs)
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-    def __str__(self):
-        return self.email
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class UserRegistration(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME
