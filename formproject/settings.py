fa623948ed3a41cc3dcbc24c2aabee833f715bd7
Sachin <homesachin@M3-Max.local>
2024-01-31 12:18:20 +0530

    typing commit message is most boring part

--- formproject/settings.py
+++ formproject/settings.py
@@ -17,6 +17,11 @@ ALLOWED_HOSTS = []
 # AUTH_USER_MODEL = 'form1.User'
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 # Application definition
 
 INSTALLED_APPS = [
