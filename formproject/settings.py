abd6674147c7aff8c05fa66f07ef42b881ae6bd9
Sachin <homesachin@M3-Max.local>
2024-02-05 08:31:17 +0530

    Modified: orm1/__pycache__/urls.cpython-312.pyc

--- formproject/settings.py
+++ formproject/settings.py
@@ -50,9 +50,9 @@ INSTALLED_APPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nine',
-        'USER': 'root',
-        'PASSWORD': 'mastersoon',
+        'NAME': 'form1',
+        'USER': 'mona',
+        'PASSWORD': 'lisa',
         'HOST': 'localhost',
         'PORT': '3306',
     }
--- formproject/urls.py
+++ formproject/urls.py
@@ -1,5 +1,5 @@
 
-
+# adasfafsa
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
